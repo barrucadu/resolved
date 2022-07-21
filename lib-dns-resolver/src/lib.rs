c231ef906cf5f3c55837b348edfe54883bd46431
Michael Walker <mike@barrucadu.co.uk>
2022-07-21 18:26:12 +0100

    Triage `clippy::pedantic` lints

--- lib-dns-resolver/src/lib.rs
+++ lib-dns-resolver/src/lib.rs
@@ -1,3 +1,32 @@
+#![warn(clippy::pedantic)]
+// TODO: fix
+#![allow(clippy::cloned_instead_of_copied)]
+#![allow(clippy::explicit_into_iter_loop)]
+#![allow(clippy::explicit_iter_loop)]
+#![allow(clippy::if_not_else)]
+#![allow(clippy::inconsistent_struct_constructor)]
+#![allow(clippy::manual_assert)]
+#![allow(clippy::match_wildcard_for_single_variants)]
+#![allow(clippy::missing_errors_doc)]
+#![allow(clippy::missing_panics_doc)]
+#![allow(clippy::needless_pass_by_value)]
+#![allow(clippy::redundant_closure_for_method_calls)]
+#![allow(clippy::redundant_else)]
+#![allow(clippy::semicolon_if_nothing_returned)]
+#![allow(clippy::single_match)]
+#![allow(clippy::single_match_else)]
+#![allow(clippy::trivially_copy_pass_by_ref)]
+#![allow(clippy::unreadable_literal)]
+// Don't care enough to fix
+#![allow(clippy::match_same_arms)]
+#![allow(clippy::must_use_candidate)]
+#![allow(clippy::module_name_repetitions)]
+#![allow(clippy::return_self_not_must_use)]
+#![allow(clippy::similar_names)]
+#![allow(clippy::struct_excessive_bools)]
+#![allow(clippy::too_many_lines)]
+#![allow(clippy::wildcard_imports)]
+
 pub mod cache;
 pub mod forwarding;
 pub mod metrics;
--- lib-dns-types/src/lib.rs
+++ lib-dns-types/src/lib.rs
@@ -1,3 +1,28 @@
+#![warn(clippy::pedantic)]
+// TODO: fix
+#![allow(clippy::cloned_instead_of_copied)]
+#![allow(clippy::explicit_into_iter_loop)]
+#![allow(clippy::explicit_iter_loop)]
+#![allow(clippy::if_not_else)]
+#![allow(clippy::manual_assert)]
+#![allow(clippy::match_wildcard_for_single_variants)]
+#![allow(clippy::missing_errors_doc)]
+#![allow(clippy::missing_panics_doc)]
+#![allow(clippy::needless_pass_by_value)]
+#![allow(clippy::redundant_closure_for_method_calls)]
+#![allow(clippy::redundant_else)]
+#![allow(clippy::semicolon_if_nothing_returned)]
+#![allow(clippy::trivially_copy_pass_by_ref)]
+#![allow(clippy::unreadable_literal)]
+// Don't care enough to fix
+#![allow(clippy::match_same_arms)]
+#![allow(clippy::must_use_candidate)]
+#![allow(clippy::return_self_not_must_use)]
+#![allow(clippy::similar_names)]
+#![allow(clippy::struct_excessive_bools)]
+#![allow(clippy::too_many_lines)]
+#![allow(clippy::wildcard_imports)]
+
 pub mod hosts;
 pub mod protocol;
 pub mod zones;
