8deca2d892d4248095a5de8680c27811b418a2ab
Michael Walker <mike@barrucadu.co.uk>
2025-02-04 21:40:51 +0000

    Fix `clippy::doc_overindented_list_items`

--- crates/dns-types/src/protocol/types.rs
+++ crates/dns-types/src/protocol/types.rs
@@ -206,24 +206,23 @@ pub struct Header {
     /// - `0` No error condition
     ///
     /// - `1` Format error - The name server was unable to interpret
-    ///       the query.
+    ///   the query.
     ///
-    /// - `2` Server failure - The name server was unable to process
-    ///       this query due to a problem with the name server.
+    /// - `2` Server failure - The name server was unable to process this query
+    ///   due to a problem with the name server.
     ///
-    /// - `3` Name Error - Meaningful only for responses from an
-    ///       authoritative name server, this code signifies that the
-    ///       domain name referenced in the query does not exist.
+    /// - `3` Name Error - Meaningful only for responses from an authoritative
+    ///   name server, this code signifies that the domain name referenced in
+    ///   the query does not exist.
     ///
-    /// - `4` Not Implemented - The name server does not support the
-    ///       requested kind of query.
+    /// - `4` Not Implemented - The name server does not support the requested
+    ///   kind of query.
     ///
-    /// - `5` Refused - The name server refuses to perform the
-    ///       specified operation for policy reasons.  For example, a
-    ///       name server may not wish to provide the information to
-    ///       the particular requester, or a name server may not wish
-    ///       to perform a particular operation (e.g., zone transfer)
-    ///       for particular data.
+    /// - `5` Refused - The name server refuses to perform the specified
+    ///   operation for policy reasons.  For example, a name server may not wish
+    ///   to provide the information to the particular requester, or a name
+    ///   server may not wish to perform a particular operation (e.g., zone
+    ///   transfer) for particular data.
     ///
     /// - `6-15` Reserved for future use.
     pub rcode: Rcode,
